7a78a2a2d497b5346037863be88c742fad19cd00
Maxime Beaudoin <maxime.beaudoin@ellipse-synergie.com>
2026-04-19 09:07:38 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/boxflow/",
   plugins: [react(), tailwindcss()],
 });
